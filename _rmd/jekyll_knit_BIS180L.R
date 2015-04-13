7c3a75580096ace35993bbd6978553c5375100f1
Julin Maloof <jnmaloof@ucdavis.edu>
2015-04-13 12:39:57 -0700

    updated rmd instructions and knitr file

--- _rmd/jekyll_knit_BIS180L.R
+++ _rmd/jekyll_knit_BIS180L.R
@@ -1,15 +1,17 @@
-setwd("~/git/BIS180L_web/_posts/")
+setwd("~/git/BIS180L_web/")
 library(knitr)
-
 
 jekyll.knit <- function(input) {
     rel.path = substr(input, 5, nchar(input) - 4)
     output = paste("_posts/", rel.path, ".md", sep = "")
     knit(input = input, output = output)
-    system(paste("perl -pi -e 's|image/|{{ site.figurl }}/|gi'", output, sep = " "))
+    system(paste("perl -pi -e 's|images/|{{ site.baseurl }}/images/|gi'", output, sep = " ")) # for non-R generated figures
+    system(paste("perl -pi -e 's|figure/|{{ site.baseurl }}/figure/|gi'", output, sep = " ")) # for R figures generated during knitting
+    
+    
 }
 # .Rmd files in _rmd directory
 # Usage example:
 # jekyll.knit("_rmd/2013-XX-XX-post-name.Rmd")
- jekyll.knit("_rmd/2013-05-01-Text-Searches-Regular-Expressions-Answers.Rmd")
+ jekyll.knit("_rmd/2015-04-15-test.Rmd")
 
