3b4244f43d01ba8485aea7a6edb9e51afd4fc362
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-05-18 23:14:50 -0700

    Genetic Networks 98%, doubble check paths to data sets on virtual machine

--- _rmd/jekyll_knit_BIS180L.R
+++ _rmd/jekyll_knit_BIS180L.R
@@ -1,24 +1,16 @@
-setwd("~/git.repos/BIS180L_web/_rmd")
+setwd("~/git.repos/BIS180L_web/")
 library(knitr)
 
 jekyll.knit <- function(input) {
-    rel.path = substr(input, 5, nchar(input) - 4)
+    rel.path = substr(input, 6, nchar(input) - 4)
     output = paste("_posts/", rel.path, ".md", sep = "")
     knit(input = input, output = output)
-    #system(paste("perl -pi -e 's|images/|{{ site.baseurl }}/images/|gi'", output, sep = " ")) # for non-R generated figures
-    #system(paste("perl -pi -e 's|figure/|{{ site.baseurl }}/figure/|gi'", output, sep = " ")) # for R figures generated during knitting
+    system(paste("perl -pi -e 's|images/|{{ site.baseurl }}/images/|gi'", output, sep = " ")) # for non-R generated figures
+    system(paste("perl -pi -e 's|figure/|{{ site.baseurl }}/figure/|gi'", output, sep = " ")) # for R figures generated during knitting
     
     
 }
 # .Rmd files in _rmd directory
 # Usage example:
 # jekyll.knit("_rmd/2013-XX-XX-post-name.Rmd")
-jekyll.knit("2015-05-19-genetic-networks1.Rmd")
-
-rel.path = substr("_rmd/2015-05-19-genetic-networks-1.Rmd", 5, nchar("_rmd/2015-05-19-genetic-networks-1.Rmd") - 5)
-rel.path
-
-?knit
-
-
-knit(input = "~/git.repos/BIS180L_web/_rmd/2015-05-19-genetic-networks-1.Rmd", output = "~/git.repos/BIS180L_web/_posts/2015-05-19-genetic-networks-1.md")
+jekyll.knit("_rmd/2015-05-19-genetic-networks-1.Rmd")
